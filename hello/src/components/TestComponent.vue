80dde52cac8e19ca708134105725594198d489af
Bryce-Soghigian <brybrycodingguy@gmail.com>
2021-02-17 20:48:02 -0600

    learned basic hello world stuff

---
+++ hello/src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <h1>Hello {{username}}</h1>
+    <h3>Welcome to my first vue app </h3> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TestComponent",
+    props: {
+        username:String
+    }
+}
+
+</script>
